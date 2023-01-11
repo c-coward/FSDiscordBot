2ca80c0790fd7b278c964f81e41b551f33092679
Chloe Coward <jrc0114@auburn.edu>
2023-01-11 12:06:00 -0600

    reimplemented dice rolls with FParsec

--- src/cmd/Dice.fs
+++ src/cmd/Dice.fs
@@ -7,6 +7,7 @@ open DSharpPlus.CommandsNext.Attributes
 open DSharpPlus.Entities
 
 open MusicBot.Util.Parsing
+open FParsec
 
 type Dice () =
     inherit BaseCommandModule ()
@@ -16,7 +17,7 @@ type Dice () =
     [<Description "Roll the dice">]
     member this.Roll(ctx: CommandContext, [<RemainingText>] txt) : Task = task {
         match run roll txt with
-        | Success ((nums, m), _) ->
+        | Success ((nums, m), _, _) ->
             let result = Seq.sum nums + m
             let combined = String.Join(" + ", Seq.map string nums)
             let embed = DiscordEmbedBuilder()
@@ -24,5 +25,10 @@ type Dice () =
             let modString = if m = 0 then "" else $" + {m}"
             embed.Description <- $"( {combined} ){modString} -> {result}"
             ctx.RespondAsync(embed) |> ignore
-        | _ -> ()
+        | Failure _ ->
+            let embed = DiscordEmbedBuilder()
+            embed.Color <- DiscordColor.Purple
+            embed.Description <- "I can't read your roll. Make sure it looks like \
+            \"[x] d [y] + [z]\", but you don't need a modifier and I can ignore case/whitespace."
+            ctx.RespondAsync(embed) |> ignore
     }
